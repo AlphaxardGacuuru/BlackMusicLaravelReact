d89c60fe7d2e6b86768f9cff0a46f49a4fde1498
Alphaxard Gacuuru <alphaxardgacuuru47@gmail.com>
2021-09-25 22:34:06 +0300

    t

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -15,8 +15,8 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $middleware = [
-        // \Illuminate\Session\Middleware\StartSession::class,
-        // \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+        \Illuminate\Session\Middleware\StartSession::class,
+        \Illuminate\View\Middleware\ShareErrorsFromSession::class,
 
         \App\Http\Middleware\TrustHosts::class,
         \App\Http\Middleware\TrustProxies::class,
