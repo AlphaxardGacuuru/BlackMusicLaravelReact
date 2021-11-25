0ce9a61ca53d195515c7ec0dc632f17b79bd58a3
Alphaxard Gacuuru <alphaxardgacuuru47@gmail.com>
2021-11-25 16:12:23 +0300

    test

--- app/Http/Controllers/KopokopoController.php
+++ app/Http/Controllers/KopokopoController.php
@@ -97,8 +97,8 @@ class KopokopoController extends Controller
         $kopokopo->kopokopo_id = $request->data['id'];
         $kopokopo->type = $request->data['type'];
         $kopokopo->initiationTime = $request->data['attributes']['initiation_time'];
-        // $kopokopo->status = $request->data['status'];
-        // $kopokopo->eventType = $request->data['eventType'];
+        $kopokopo->status = $request->data['attributes']['status'];
+        $kopokopo->eventType = $request->data['attributes']['event']['type'];
         // $kopokopo->resourceId = $request->data['resourceId'];
         // $kopokopo->reference = $request->data['reference'];
         // $kopokopo->originationTime = $request->data['originationTime'];
