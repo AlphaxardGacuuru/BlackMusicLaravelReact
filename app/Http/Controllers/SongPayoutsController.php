89fbd641bd7d5fd27caf03530ff0af03cd9389f0
Alphaxard Gacuuru <alphaxardgacuuru47@gmail.com>
2022-02-18 20:01:35 +0300

    test

--- app/Http/Controllers/SongPayoutsController.php
+++ app/Http/Controllers/SongPayoutsController.php
@@ -104,7 +104,8 @@ class SongPayoutsController extends Controller
         $response = $pay->sendPay([
             'destinationType' => 'mobile_wallet',
             'destinationReference' => $request->input('destination_reference'),
-            'amount' => $amount > 1000 ? $amount : $amount - 50,
+            // 'amount' => $amount > 1000 ? $amount : $amount - 50,
+            'amount' => $amount,
             'currency' => 'KES',
             'callbackUrl' => 'https://music.black.co.ke/api/song-payouts',
             'description' => 'Song Payout',
