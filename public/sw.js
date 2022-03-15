16c27be84ad5d3aca5c36e7d1a3d5f8dc3bd08e7
Alphaxard Gacuuru <alphaxardgacuuru47@gmail.com>
2022-03-16 00:11:20 +0300

    updated sw

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 // cacheName needs to be changed after any of the assets are changed
-const cacheName = 'BlackMusicReactLaravelCache9';
+const cacheName = 'BlackMusicReactLaravelCache1';
 
 const cacheAssets = [
 	'/js/app.js'
