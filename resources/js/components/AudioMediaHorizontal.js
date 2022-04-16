6490c1ccc92bd60d229c90dbc1107933b4808219
Alphaxard Gacuuru <alphaxardgacuuru47@gmail.com>
2022-04-16 14:48:27 +0300

    increased audio max width

--- resources/js/components/AudioMediaHorizontal.js
+++ resources/js/components/AudioMediaHorizontal.js
@@ -41,7 +41,7 @@ const AudioMediaHorizontal = (props) => {
 					<h6
 						className="mb-0 pb-0"
 						style={{
-							maxWidth: "9em",
+							maxWidth: "8em",
 							whiteSpace: "nowrap",
 							overflow: "hidden",
 							textOverflow: "clip"
