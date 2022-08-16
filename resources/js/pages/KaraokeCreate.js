2ead5887997b397e49794787babd354a3493b453
Alphaxard Gacuuru <alphaxardgacuuru47@gmail.com>
2022-08-16 22:34:54 +0300

    Testing flash

--- resources/js/pages/KaraokeCreate.js
+++ resources/js/pages/KaraokeCreate.js
@@ -179,16 +179,16 @@ const KaraokeCreate = (props) => {
 
 			return imageCapture.getPhotoCapabilities();
 		}).then((photoCapabilities) => {
-			console.log(track.applyConstraints())
+
 			// Check if camera has a torch
 			if (photoCapabilities.fillLightMode) {
 				// auto, off, or flash
 				setFlash(photoCapabilities.fillLightMode)
-
-				track.applyConstraints({
-					advanced: [{ torch: true }]
-				});
 			}
+
+			track.applyConstraints({
+				advanced: [{ torch: true }]
+			});
 
 		}).catch(function (err) {
 			console.log(err.name + ": " + err.message);
@@ -237,7 +237,7 @@ const KaraokeCreate = (props) => {
 										<center>
 											<span style={{ fontSize: "1.8em" }}>
 												{!flash == "off" ? <FlashFilledSVG /> : <FlashSVG />}
-												<h6>{flash}</h6>
+												<h6>Flash</h6>
 											</span>
 										</center>
 									</div>
